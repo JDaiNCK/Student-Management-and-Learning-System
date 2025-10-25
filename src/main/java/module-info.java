72e7b68944985d350256fcff708764c4aec07fe4
unknown <juy62610@liceo.edu.ph>
2025-10-25 21:17:49 +0800

    First commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.example.studentmanagement {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.studentmanagement to javafx.fxml;
+    exports com.example.studentmanagement;
+}
